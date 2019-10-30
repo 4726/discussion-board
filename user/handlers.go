f50cb5fb884792a3d60fefd126a79298bacb20df
4726 <jun28371@gmail.com>
2019-10-29 18:43:59 -0700

    add user service tests

--- user/handlers.go
+++ user/handlers.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jinzhu/gorm"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
+	"regexp"
 	"strconv"
 	"time"
 )
@@ -31,12 +32,16 @@ type ErrorResponse struct {
 	Error string
 }
 
+var (
+	InvalidJSONBodyResponse = ErrorResponse{"invalid body"}
+)
+
 func GetProfile(db *gorm.DB, ctx *gin.Context) {
 	useridS := ctx.Param("userid")
 
 	userid, err := strconv.Atoi(useridS)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, ErrorResponse{err.Error()})
+		ctx.JSON(http.StatusBadRequest, ErrorResponse{"invalid userid param"})
 		return
 	}
 
@@ -57,13 +62,17 @@ func ValidLogin(db *gorm.DB, ctx *gin.Context) {
 	form := LoginForm{}
 	err := ctx.BindJSON(&form)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, ErrorResponse{err.Error()})
+		ctx.JSON(http.StatusBadRequest, InvalidJSONBodyResponse)
 		return
 	}
 
 	auth := Auth{}
 	if err := db.Where("username = ?", form.Username).First(&auth).Error; err != nil {
-		ctx.JSON(http.StatusInternalServerError, ErrorResponse{err.Error()})
+		if gorm.IsRecordNotFoundError(err) {
+			ctx.JSON(http.StatusUnauthorized, ErrorResponse{"invalid login"})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse{"server error"})
 		return
 	}
 
@@ -79,13 +88,33 @@ func CreateAccount(db *gorm.DB, ctx *gin.Context) {
 	form := CreateAccountForm{}
 	err := ctx.BindJSON(&form)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, ErrorResponse{err.Error()})
+		ctx.JSON(http.StatusBadRequest, InvalidJSONBodyResponse)
+		return
+	}
+
+	match, err := validUsername(form.Username)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse{"server error"})
+		return
+	}
+	if !match {
+		ctx.JSON(http.StatusBadRequest, ErrorResponse{"invalid username"})
+		return
+	}
+
+	match, err = validPassword(form.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse{"server error"})
+		return
+	}
+	if !match {
+		ctx.JSON(http.StatusBadRequest, ErrorResponse{"invalid password"})
 		return
 	}
 
 	hash, err := bcrypt.GenerateFromPassword([]byte(form.Password), bcrypt.DefaultCost)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, ErrorResponse{err.Error()})
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse{"server error"})
 		return
 	}
 
@@ -97,7 +126,7 @@ func CreateAccount(db *gorm.DB, ctx *gin.Context) {
 		UpdatedAt: created,
 	}
 	if err := db.Save(&auth).Error; err != nil {
-		ctx.JSON(http.StatusInternalServerError, ErrorResponse{err.Error()})
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse{"server error"})
 		return
 	}
 
@@ -108,7 +137,7 @@ func UpdateProfile(db *gorm.DB, ctx *gin.Context) {
 	form := UpdateProfileForm{}
 	err := ctx.BindJSON(&form)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, ErrorResponse{err.Error()})
+		ctx.JSON(http.StatusBadRequest, InvalidJSONBodyResponse)
 		return
 	}
 
@@ -133,7 +162,7 @@ func ChangePassword(db *gorm.DB, ctx *gin.Context) {
 	form := ChangePasswordForm{}
 	err := ctx.BindJSON(&form)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, ErrorResponse{err.Error()})
+		ctx.JSON(http.StatusBadRequest, InvalidJSONBodyResponse)
 		return
 	}
 
@@ -144,14 +173,18 @@ func ChangePassword(db *gorm.DB, ctx *gin.Context) {
 		}
 	}()
 	if err := tx.Error; err != nil {
-		ctx.JSON(http.StatusInternalServerError, ErrorResponse{err.Error()})
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse{"server error"})
 		return
 	}
 
 	auth := Auth{}
 	if err := tx.First(&auth, form.UserID).Error; err != nil {
 		tx.Rollback()
-		ctx.JSON(http.StatusInternalServerError, ErrorResponse{err.Error()})
+		if gorm.IsRecordNotFoundError(err) {
+			ctx.JSON(http.StatusNotFound, gin.H{})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse{"server error"})
 		return
 	}
 	if err := bcrypt.CompareHashAndPassword([]byte(auth.Password), []byte(form.OldPass)); err != nil {
@@ -160,23 +193,59 @@ func ChangePassword(db *gorm.DB, ctx *gin.Context) {
 		return
 	}
 
+	match, err := validPassword(form.NewPass)
+	if err != nil {
+		tx.Rollback()
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse{"server error"})
+		return
+	}
+	if !match {
+		tx.Rollback()
+		ctx.JSON(http.StatusBadRequest, ErrorResponse{"invalid new password"})
+		return
+	}
+
 	hash, err := bcrypt.GenerateFromPassword([]byte(form.NewPass), bcrypt.DefaultCost)
 	if err != nil {
 		tx.Rollback()
-		ctx.JSON(http.StatusInternalServerError, ErrorResponse{err.Error()})
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse{"server error"})
 		return
 	}
 
 	if err := tx.Model(&auth).Update("password", string(hash)).Error; err != nil {
 		tx.Rollback()
-		ctx.JSON(http.StatusInternalServerError, ErrorResponse{err.Error()})
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse{"server error"})
 		return
 	}
 	if err := tx.Commit().Error; err != nil {
 		tx.Rollback()
-		ctx.JSON(http.StatusInternalServerError, ErrorResponse{err.Error()})
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse{"server error"})
 		return
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{})
+}
+
+func validUsername(s string) (bool, error) {
+	match, err := regexp.MatchString("^[A-Za-z0-9]{3,64}$", s)
+	if err != nil {
+		return false, err
+	}
+	if !match {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func validPassword(s string) (bool, error) {
+	match, err := regexp.MatchString("^[A-Za-z0-9]{6,64}$", s)
+	if err != nil {
+		return false, err
+	}
+	if !match {
+		return false, nil
+	}
+
+	return true, nil
 }
---
+++ user/handlers_test.go
@@ -0,0 +1,474 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func assertJSON(t testing.TB, obj interface{}) string {
+	b, err := json.Marshal(obj)
+	assert.NoError(t, err)
+	return string(b)
+}
+
+func TestGetProfileInvalidParam(t *testing.T) {
+	cfg, err := ConfigFromJSON("config_test.json")
+	assert.NoError(t, err)
+	api, err := NewRestAPI(cfg)
+	assert.NoError(t, err)
+	api.db.Exec("TRUNCATE auths;")
+	api.db.Exec("TRUNCATE profiles;")
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/profile/a", nil)
+	api.engine.ServeHTTP(w, req)
+
+	expected := ErrorResponse{"invalid userid param"}
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+}
+
+func TestGetProfileDoesNotExist(t *testing.T) {
+	cfg, err := ConfigFromJSON("config_test.json")
+	assert.NoError(t, err)
+	api, err := NewRestAPI(cfg)
+	assert.NoError(t, err)
+	api.db.Exec("TRUNCATE auths;")
+	api.db.Exec("TRUNCATE profiles;")
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/profile/1", nil)
+	api.engine.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotFound, w.Code)
+	assert.Equal(t, "{}", w.Body.String())
+}
+
+// func TestGetProfile(t *testing.T) {
+// 	cfg, err := ConfigFromJSON("config_test.json")
+// 	assert.NoError(t, err)
+// 	api, err := NewRestAPI(cfg)
+// 	assert.NoError(t, err)
+// api.db.Exec("TRUNCATE auths;")
+// api.db.Exec("TRUNCATE profiles;")
+
+// 	//create account
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("GET", "/profile/a", nil)
+// 	api.engine.ServeHTTP(w, req)
+
+// 	expected := Profile {
+// 		UserID: 1,
+// 		Username: username,
+// 		Bio: "",
+// 		AvatarID: "",
+// 	}
+
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+// }
+
+func TestValidLoginWrongBodyFormat(t *testing.T) {
+	cfg, err := ConfigFromJSON("config_test.json")
+	assert.NoError(t, err)
+	api, err := NewRestAPI(cfg)
+	assert.NoError(t, err)
+	api.db.Exec("TRUNCATE auths;")
+	api.db.Exec("TRUNCATE profiles;")
+
+	buffer := bytes.NewBuffer([]byte("1"))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/login", buffer)
+	api.engine.ServeHTTP(w, req)
+
+	expected := InvalidJSONBodyResponse
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+}
+
+func TestValidLoginUsernameDoesNotExist(t *testing.T) {
+	cfg, err := ConfigFromJSON("config_test.json")
+	assert.NoError(t, err)
+	api, err := NewRestAPI(cfg)
+	assert.NoError(t, err)
+	api.db.Exec("TRUNCATE auths;")
+	api.db.Exec("TRUNCATE profiles;")
+
+	form := LoginForm{"username", "password"}
+	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/login", buffer)
+	api.engine.ServeHTTP(w, req)
+
+	expected := ErrorResponse{"invalid login"}
+
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+}
+
+// func TestValidLoginWrongPassword(t *testing.T) {
+// 	e.POST("/login").WithJSON(form)
+// 		Expect().
+// 		Status(http.StatusBadRequest).
+// 		JSON().Object().Equal(expected)
+
+// 		cfg, err := ConfigFromJSON("config_test.json")
+// 		assert.NoError(t, err)
+// 		api, err := NewRestAPI(cfg)
+// 		assert.NoError(t, err)
+// 		api.db.Exec("TRUNCATE auths;")
+// 		api.db.Exec("TRUNCATE profiles;")
+
+// 		//create account username password
+
+// 		form := LoginForm{"username", "passwor"}
+// 		buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+// 		w := httptest.NewRecorder()
+// 		req, _ := http.NewRequest("POST", "/login", buffer)
+// 		api.engine.ServeHTTP(w, req)
+
+// 		expected := ErrorResponse{"invalid login"}
+
+// 		assert.Equal(t, http.StatusUnauthorized, w.Code)
+// 		assert.Equal(t, assertJSON(t, expected), w.Body.String())
+// }
+
+// func TestValidLogin(t *testing.T) {
+// 	cfg, err := ConfigFromJSON("config_test.json")
+// 	assert.NoError(t, err)
+// 	api, err := NewRestAPI(cfg)
+// 	assert.NoError(t, err)
+// 	api.db.Exec("TRUNCATE auths;")
+// 	api.db.Exec("TRUNCATE profiles;")
+
+// 	//create account username password
+
+// 	form := LoginForm{"username", "password"}
+// 	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("POST", "/login", buffer)
+// 	api.engine.ServeHTTP(w, req)
+
+// 	expected := map[string]interface{}{"userid": 1}
+
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+// }
+
+func TestCreateAccountWrongBodyFormat(t *testing.T) {
+	cfg, err := ConfigFromJSON("config_test.json")
+	assert.NoError(t, err)
+	api, err := NewRestAPI(cfg)
+	assert.NoError(t, err)
+	api.db.Exec("TRUNCATE auths;")
+	api.db.Exec("TRUNCATE profiles;")
+
+	buffer := bytes.NewBuffer([]byte("1"))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/account", buffer)
+	api.engine.ServeHTTP(w, req)
+
+	expected := InvalidJSONBodyResponse
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+}
+
+func TestCreateAccountInvalidUsername(t *testing.T) {
+	cfg, err := ConfigFromJSON("config_test.json")
+	assert.NoError(t, err)
+	api, err := NewRestAPI(cfg)
+	assert.NoError(t, err)
+	api.db.Exec("TRUNCATE auths;")
+	api.db.Exec("TRUNCATE profiles;")
+
+	form := CreateAccountForm{"us", "password"}
+	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/account", buffer)
+	api.engine.ServeHTTP(w, req)
+
+	expected := ErrorResponse{"invalid username"}
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+}
+
+func TestCreateAccountInvalidPassword(t *testing.T) {
+	cfg, err := ConfigFromJSON("config_test.json")
+	assert.NoError(t, err)
+	api, err := NewRestAPI(cfg)
+	assert.NoError(t, err)
+	api.db.Exec("TRUNCATE auths;")
+	api.db.Exec("TRUNCATE profiles;")
+
+	form := CreateAccountForm{"username", "passw"}
+	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/account", buffer)
+	api.engine.ServeHTTP(w, req)
+
+	expected := ErrorResponse{"invalid password"}
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+}
+
+func TestCreateAccount(t *testing.T) {
+	cfg, err := ConfigFromJSON("config_test.json")
+	assert.NoError(t, err)
+	api, err := NewRestAPI(cfg)
+	assert.NoError(t, err)
+	api.db.Exec("TRUNCATE auths;")
+	api.db.Exec("TRUNCATE profiles;")
+
+	form := CreateAccountForm{"username", "password"}
+	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/account", buffer)
+	api.engine.ServeHTTP(w, req)
+
+	expected := map[string]interface{}{"userid": 1}
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+
+	form = CreateAccountForm{"username2", "password2"}
+	buffer = bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("POST", "/account", buffer)
+	api.engine.ServeHTTP(w, req)
+
+	expected = map[string]interface{}{"userid": 2}
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+}
+
+func TestUpdateProfileWrongBodyFormat(t *testing.T) {
+	cfg, err := ConfigFromJSON("config_test.json")
+	assert.NoError(t, err)
+	api, err := NewRestAPI(cfg)
+	assert.NoError(t, err)
+	api.db.Exec("TRUNCATE auths;")
+	api.db.Exec("TRUNCATE profiles;")
+
+	buffer := bytes.NewBuffer([]byte("1"))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/profile/update", buffer)
+	api.engine.ServeHTTP(w, req)
+
+	expected := InvalidJSONBodyResponse
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+}
+
+// func TestUpdateProfileNone(t *testing.T) {
+// 	cfg, err := ConfigFromJSON("config_test.json")
+// 	assert.NoError(t, err)
+// 	api, err := NewRestAPI(cfg)
+// 	assert.NoError(t, err)
+// 	api.db.Exec("TRUNCATE auths;")
+// 	api.db.Exec("TRUNCATE profiles;")
+
+// 	//create account
+
+// 	form := UpdateProfileForm{1, "", ""}
+// 	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("POST", "/profile/update", buffer)
+// 	api.engine.ServeHTTP(w, req)
+
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Equal(t, "{}", w.Body.String())
+// }
+
+// func TestUpdateProfileBio(t *testing.T) {
+// 	cfg, err := ConfigFromJSON("config_test.json")
+// 	assert.NoError(t, err)
+// 	api, err := NewRestAPI(cfg)
+// 	assert.NoError(t, err)
+// 	api.db.Exec("TRUNCATE auths;")
+// 	api.db.Exec("TRUNCATE profiles;")
+
+// 	//create account
+
+// 	form := UpdateProfileForm{1, "hello world", ""}
+// 	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("POST", "/profile/update", buffer)
+// 	api.engine.ServeHTTP(w, req)
+
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Equal(t, "{}", w.Body.String())
+// }
+
+// func TestUpdateProfileAvatarID(t *testing.T) {
+// 	cfg, err := ConfigFromJSON("config_test.json")
+// 	assert.NoError(t, err)
+// 	api, err := NewRestAPI(cfg)
+// 	assert.NoError(t, err)
+// 	api.db.Exec("TRUNCATE auths;")
+// 	api.db.Exec("TRUNCATE profiles;")
+
+// 	//create account
+
+// 	form := UpdateProfileForm{1, "", "a"}
+// 	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("POST", "/profile/update", buffer)
+// 	api.engine.ServeHTTP(w, req)
+
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Equal(t, "{}", w.Body.String())
+// }
+
+// func TestUpdateProfile(t *testing.T) {
+// 	cfg, err := ConfigFromJSON("config_test.json")
+// 	assert.NoError(t, err)
+// 	api, err := NewRestAPI(cfg)
+// 	assert.NoError(t, err)
+// 	api.db.Exec("TRUNCATE auths;")
+// 	api.db.Exec("TRUNCATE profiles;")
+
+// 	//create account
+
+// 	form := UpdateProfileForm{1, "hello world", "a"}
+// 	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("POST", "/profile/update", buffer)
+// 	api.engine.ServeHTTP(w, req)
+
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Equal(t, "{}", w.Body.String())
+// }
+
+func TestChangePasswordWrongBodyFormat(t *testing.T) {
+	cfg, err := ConfigFromJSON("config_test.json")
+	assert.NoError(t, err)
+	api, err := NewRestAPI(cfg)
+	assert.NoError(t, err)
+	api.db.Exec("TRUNCATE auths;")
+	api.db.Exec("TRUNCATE profiles;")
+
+	//create account
+
+	buffer := bytes.NewBuffer([]byte("1"))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/password", buffer)
+	api.engine.ServeHTTP(w, req)
+
+	expected := InvalidJSONBodyResponse
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+}
+
+func TestChangePasswordUserDoesNotExist(t *testing.T) {
+	cfg, err := ConfigFromJSON("config_test.json")
+	assert.NoError(t, err)
+	api, err := NewRestAPI(cfg)
+	assert.NoError(t, err)
+	api.db.Exec("TRUNCATE auths;")
+	api.db.Exec("TRUNCATE profiles;")
+
+	form := ChangePasswordForm{1, "password", "newpassword"}
+	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/password", buffer)
+	api.engine.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotFound, w.Code)
+	assert.Equal(t, "{}", w.Body.String())
+}
+
+// func TestChangePasswordInvalidOld(t *testing.T) {
+// 	cfg, err := ConfigFromJSON("config_test.json")
+// 	assert.NoError(t, err)
+// 	api, err := NewRestAPI(cfg)
+// 	assert.NoError(t, err)
+// 	api.db.Exec("TRUNCATE auths;")
+// 	api.db.Exec("TRUNCATE profiles;")
+
+// 	//add username password
+
+// 	form := ChangePasswordForm{1, "passwor", "newpassword"}
+// 	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("POST", "/password", buffer)
+// 	api.engine.ServeHTTP(w, req)
+
+// 	expected := ErrorResponse{"invalid old password"}
+
+// 	assert.Equal(t, http.StatusUnauthorized, w.Code)
+// 	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+// }
+
+// func TestChangePasswordInvalidNew(t *testing.T) {
+// 	cfg, err := ConfigFromJSON("config_test.json")
+// 	assert.NoError(t, err)
+// 	api, err := NewRestAPI(cfg)
+// 	assert.NoError(t, err)
+// 	api.db.Exec("TRUNCATE auths;")
+// 	api.db.Exec("TRUNCATE profiles;")
+
+// 	//add username password
+
+// 	form := ChangePasswordForm{1, "password", "newpa"}
+// 	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("POST", "/password", buffer)
+// 	api.engine.ServeHTTP(w, req)
+
+// 	expected := ErrorResponse{"invalid new password"}
+
+// 	assert.Equal(t, http.StatusBadRequest, w.Code)
+// 	assert.Equal(t, assertJSON(t, expected), w.Body.String())
+// }
+
+// func TestChangePassword(t *testing.T) {
+// 	cfg, err := ConfigFromJSON("config_test.json")
+// 	assert.NoError(t, err)
+// 	api, err := NewRestAPI(cfg)
+// 	assert.NoError(t, err)
+// 	api.db.Exec("TRUNCATE auths;")
+// 	api.db.Exec("TRUNCATE profiles;")
+
+// 	//add username password
+
+// 	form := ChangePasswordForm{1, "password", "newpassword"}
+// 	buffer := bytes.NewBuffer([]byte(assertJSON(t, form)))
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("POST", "/password", buffer)
+// 	api.engine.ServeHTTP(w, req)
+
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Equal(t, "{}", w.Body.String())
+// }
