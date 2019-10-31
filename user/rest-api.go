0afa680dcb4dee927f4c66132fd2fd18551efa9d
4726 <jun28371@gmail.com>
2019-10-31 13:44:04 -0700

    add pull monitor service

--- user/rest-api.go
+++ user/rest-api.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type RestAPI struct {
@@ -19,6 +20,7 @@ func NewRestAPI(cfg Config) (*RestAPI, error) {
 	gin.SetMode(gin.ReleaseMode)
 	api.engine = engine
 	api.setRoutes()
+	api.setMonitorRoute()
 
 	s := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", cfg.Username, cfg.Password, cfg.Addr, cfg.DBName)
 
@@ -53,6 +55,10 @@ func (a *RestAPI) setRoutes() {
 	a.engine.POST("/password", func(ctx *gin.Context) {
 		ChangePassword(a.db, ctx)
 	})
+}
+
+func (a *RestAPI) setMonitorRoute() {
+	a.engine.Any("/metrics", gin.WrapH(promhttp.Handler()))
 }
 
 func (a *RestAPI) Run(addr string) error {
