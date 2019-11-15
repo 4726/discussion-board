cbda55e997cfe773de34daf78d508a2387ec2275
4726 <jun28371@gmail.com>
2019-11-15 14:00:34 -0800

    common log middleware

--- services/common/log.go
+++ services/common/log.go
@@ -1,12 +1,10 @@
 package common
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"os"
-	"runtime"
-	"strings"
+	"time"
 )
 
 var log = logrus.New()
@@ -19,32 +17,15 @@ func NewLogger(serviceName string) *Log {
 	entry := log.WithFields(logrus.Fields{
 		"service": serviceName,
 	})
+	log.SetFormatter(&logrus.JSONFormatter{})
 
-	prettyfier := func(f *runtime.Frame) (string, string) {
-		var shortFileName, shortFunctionName string
-		tokens := strings.Split(f.File, "/github.com/")
-		if len(tokens) > 1 {
-			shortFileName = fmt.Sprintf("github.com/%s:%v", tokens[1], f.Line)
-		}
-		tokens = strings.Split(f.Function, "/")
-		if len(tokens) > 1 {
-			shortFunctionName = tokens[len(tokens)-1]
-		}
-		return shortFunctionName, shortFileName
-	}
-
-	log.SetFormatter(&logrus.JSONFormatter{
-		CallerPrettyfier: prettyfier,
-	})
-	log.SetReportCaller(true)
-
-	file, err := os.OpenFile(fmt.Sprintf("logs/%s.log", serviceName), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err == nil {
-		log.SetOutput(file)
-	} else {
-		entry.Error(err)
+	// file, err := os.OpenFile(fmt.Sprintf("logs/%s.log", serviceName), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	// if err == nil {
+	// 	log.SetOutput(file)
+	// } else {
+	// 	entry.Error(err)
 		log.SetOutput(os.Stderr)
-	}
+	// }
 
 	return &Log{entry}
 }
@@ -53,8 +34,44 @@ func (l *Log) Entry() *logrus.Entry {
 	return l.entry
 }
 
-func (l *Log) HTTPRequestEntry(ctx *gin.Context) *logrus.Entry {
-	return l.entry.
-		WithField("from", ctx.ClientIP()).
-		WithField("statusCode", ctx.Writer.Status())
+func (l *Log) RequestMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+
+		c.Next()
+
+		finish := time.Now()
+
+		statusCode := c.Writer.Status()
+
+		logMessage := ""
+		i, ok := c.Get("log info")
+		if ok {
+			switch v := i.(type) {
+			case string:
+				logMessage = v
+			case error:
+				logMessage = v.Error()
+			default:
+			}
+		}
+
+		e := l.entry.WithFields(logrus.Fields{
+			"StatusCode": statusCode,
+			"ClientIP":  c.ClientIP(),
+			"Method": c.Request.Method,
+			"Path": c.Request.URL.Path,
+			"Message": logMessage,
+			"Latency": finish.Sub(start),
+		})
+
+		switch v := statusCode; {
+		case v < 400:
+			e.Info()
+		case v < 500:
+			e.Warn()
+		default:
+			e.Fatal()
+		}
+	}
 }
